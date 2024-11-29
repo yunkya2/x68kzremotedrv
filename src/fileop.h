86f182967295c0d0e032456de6d629d1b6725996
Yuichi Nakamura <y.512.nakamura@gmail.com>
2024-11-29 22:18:35 +0900

    update fileop.h
    (cherry picked from commit a132c53c729669da4c5c56597ce3c1b34b2b2bbe)

--- src/fileop.h
+++ src/fileop.h
@@ -119,7 +119,7 @@ static inline int FUNC_ATTR_FILEMODE(int attr, TYPE_STAT *st)
   return 0;   /* TBD */
 }
 
-static inline int FUNC_CHMOD(int *err, const char *path, int mode)
+static inline int FUNC_CHMOD(int unit, int *err, const char *path, int mode)
 {
   return 0;   /* TBD */
 }
@@ -128,7 +128,7 @@ static inline int FUNC_CHMOD(int *err, const char *path, int mode)
 // Filesystem operations
 //****************************************************************************
 
-static inline int FUNC_STAT(int *err, const char *path, TYPE_STAT *st)
+static inline int FUNC_STAT(int unit, int *err, const char *path, TYPE_STAT *st)
 {
   const char *shpath;
   struct smb2_context *smb2 = path2smb2(path, &shpath);
@@ -137,7 +137,7 @@ static inline int FUNC_STAT(int *err, const char *path, TYPE_STAT *st)
     *err = -r;
   return r;
 }
-static inline int FUNC_MKDIR(int *err, const char *path)
+static inline int FUNC_MKDIR(int unit, int *err, const char *path)
 {
   const char *shpath;
   struct smb2_context *smb2 = path2smb2(path, &shpath);
@@ -146,7 +146,7 @@ static inline int FUNC_MKDIR(int *err, const char *path)
     *err = -r;
   return r;
 }
-static inline int FUNC_RMDIR(int *err, const char *path)
+static inline int FUNC_RMDIR(int unit, int *err, const char *path)
 {
   const char *shpath;
   struct smb2_context *smb2 = path2smb2(path, &shpath);
@@ -155,7 +155,7 @@ static inline int FUNC_RMDIR(int *err, const char *path)
     *err = -r;
   return r;
 }
-static inline int FUNC_RENAME(int *err, const char *pathold, const char *pathnew)
+static inline int FUNC_RENAME(int unit, int *err, const char *pathold, const char *pathnew)
 {
   const char *shpath;
   const char *shpath2;
@@ -166,7 +166,7 @@ static inline int FUNC_RENAME(int *err, const char *pathold, const char *pathnew
     *err = -r;
   return r;
 }
-static inline int FUNC_UNLINK(int *err, const char *path)
+static inline int FUNC_UNLINK(int unit, int *err, const char *path)
 {
   const char *shpath;
   struct smb2_context *smb2 = path2smb2(path, &shpath);
@@ -180,7 +180,7 @@ static inline int FUNC_UNLINK(int *err, const char *path)
 // Directory operations
 //****************************************************************************
 
-static inline TYPE_DIR FUNC_OPENDIR(int *err, const char *path)
+static inline TYPE_DIR FUNC_OPENDIR(int unit, int *err, const char *path)
 {
   union smb2dd dir;
   const char *shpath;
@@ -193,14 +193,14 @@ static inline TYPE_DIR FUNC_OPENDIR(int *err, const char *path)
     *err = nterror_to_errno(smb2_get_nterror(smb2));
   return dir.dd;
 }
-static inline TYPE_DIRENT *FUNC_READDIR(int *err, TYPE_DIR dir)
+static inline TYPE_DIRENT *FUNC_READDIR(int unit, int *err, TYPE_DIR dir)
 {
   TYPE_DIRENT *d = smb2_readdir(dir2smb2(dir), dir2dir(dir));
   if (err)
     *err = nterror_to_errno(smb2_get_nterror(dir2smb2(dir)));
   return d;
 }
-static inline int FUNC_CLOSEDIR(int *err, TYPE_DIR dir)
+static inline int FUNC_CLOSEDIR(int unit, int *err, TYPE_DIR dir)
 { 
   smb2_closedir(dir2smb2(dir), dir2dir(dir));
   return 0;
@@ -210,7 +210,7 @@ static inline int FUNC_CLOSEDIR(int *err, TYPE_DIR dir)
 // File operations
 //****************************************************************************
 
-static inline TYPE_FD FUNC_OPEN(int *err, const char *path, int flags)
+static inline TYPE_FD FUNC_OPEN(int unit, int *err, const char *path, int flags)
 {
   union smb2fd fd = { .fd = FD_BADFD };
   const char *shpath;
@@ -223,21 +223,21 @@ static inline TYPE_FD FUNC_OPEN(int *err, const char *path, int flags)
     *err = nterror_to_errno(smb2_get_nterror(smb2));
   return fd.fd;
 }
-static inline int FUNC_CLOSE(int *err, TYPE_FD fd)
+static inline int FUNC_CLOSE(int unit, int *err, TYPE_FD fd)
 {
   int r = smb2_close(fd2smb2(fd), fd2sfh(fd));
   if (err)
     *err = -r;
   return r;
 }
-static inline ssize_t FUNC_READ(int *err, TYPE_FD fd, void *buf, size_t count)
+static inline ssize_t FUNC_READ(int unit, int *err, TYPE_FD fd, void *buf, size_t count)
 {
   ssize_t r = smb2_read(fd2smb2(fd), fd2sfh(fd), buf, count);
   if (err)
     *err = -r;
   return r;
 }
-static inline ssize_t FUNC_WRITE(int *err, TYPE_FD fd, const void *buf, size_t count)
+static inline ssize_t FUNC_WRITE(int unit, int *err, TYPE_FD fd, const void *buf, size_t count)
 {
   ssize_t res = 0;
   while (count > 0) {
@@ -257,14 +257,14 @@ static inline ssize_t FUNC_WRITE(int *err, TYPE_FD fd, const void *buf, size_t c
     *err = -res;
   return res;
 }
-static inline int FUNC_FTRUNCATE(int *err, TYPE_FD fd, off_t length)
+static inline int FUNC_FTRUNCATE(int unit, int *err, TYPE_FD fd, off_t length)
 {
   int r = smb2_ftruncate(fd2smb2(fd), fd2sfh(fd), length);
   if (err)
     *err = -r;
   return r;
 }
-static inline off_t FUNC_LSEEK(int *err, TYPE_FD fd, off_t offset, int whence)
+static inline off_t FUNC_LSEEK(int unit, int *err, TYPE_FD fd, off_t offset, int whence)
 {
   uint64_t cur;
   off_t r = smb2_lseek(fd2smb2(fd), fd2sfh(fd), offset, whence, &cur);
@@ -272,7 +272,7 @@ static inline off_t FUNC_LSEEK(int *err, TYPE_FD fd, off_t offset, int whence)
     *err = -r;
   return r;
 }
-static inline int FUNC_FSTAT(int *err, TYPE_FD fd, TYPE_STAT *st)
+static inline int FUNC_FSTAT(int unit, int *err, TYPE_FD fd, TYPE_STAT *st)
 {
   int r = smb2_fstat(fd2smb2(fd), fd2sfh(fd), st);
   if (err)
@@ -280,7 +280,7 @@ static inline int FUNC_FSTAT(int *err, TYPE_FD fd, TYPE_STAT *st)
   return r;
 }
 
-static inline int FUNC_FILEDATE(int *err, TYPE_FD fd, uint16_t time, uint16_t date)
+static inline int FUNC_FILEDATE(int unit, int *err, TYPE_FD fd, uint16_t time, uint16_t date)
 {
   return 0;   /* TBD */
 }
@@ -289,7 +289,7 @@ static inline int FUNC_FILEDATE(int *err, TYPE_FD fd, uint16_t time, uint16_t da
 // Misc functions
 //****************************************************************************
 
-static inline int FUNC_STATFS(int *err, const char *path, uint64_t *total, uint64_t *free)
+static inline int FUNC_STATFS(int unit, int *err, const char *path, uint64_t *total, uint64_t *free)
 {
   struct smb2_statvfs sf;
   const char *shpath;
